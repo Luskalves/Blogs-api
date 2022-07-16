7dad00b0b4614b2bff6cc09a72ec7f212ccf6c40
Luskalves <lusca.alves32@gmail.com>
2022-07-16 10:02:35 -0300

    requisito 7 concluído

---
+++ src/database/models/category.js
@@ -0,0 +1,15 @@
+const Category = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      primaryKey: true,
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+    },
+    name: DataTypes.STRING,
+  },
+  { timestamps: false });
+  
+  return Category;
+}
+
+module.exports = Category;
